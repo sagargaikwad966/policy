bdf92e9561c508f9ac5ba56cf88afadbc251b382
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-08-01 19:14:13 +0530

    all cover

--- src/test/java/com/policy/serviceImpl/UserServiceImplTest.java
+++ src/test/java/com/policy/serviceImpl/UserServiceImplTest.java
@@ -3,7 +3,10 @@ package com.policy.serviceImpl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -14,10 +17,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.itextpdf.text.DocumentException;
 import com.policy.entity.Policy;
 import com.policy.entity.User;
 import com.policy.entity.UserPolicy;
 import com.policy.exception.PolicyException;
+import com.policy.model.PolicyModel;
 import com.policy.model.UserPolicyModel;
 import com.policy.repository.UserPolicyRepository;
 import com.policy.repository.UserRepository;
@@ -47,9 +52,13 @@ public class UserServiceImplTest
 	
 	Policy policy = new Policy();
 	
+	PolicyModel policyModel = new PolicyModel();
+	
 	UserPolicy userPolicy = new UserPolicy();
 	
 	UserPolicyModel userPolicyModel = new UserPolicyModel();
+	
+	List<UserPolicyModel> userPolicyModelList = new ArrayList<>();
 	
 	Optional<User> optionalUser = Optional.of(user);
 	
@@ -61,6 +70,24 @@ public class UserServiceImplTest
 		
 		policy.setPolicyId("POL_CRP_3");
 		policy.setAgeLimit("18-70");
+		policy.setPolicyName("LIFE INSURANCE");
+		policy.setMaturityPeriod(5);
+		policy.setPremium(400.00);
+		policy.setStatus("ACTIVE");
+		policy.setSumInsured(500000.00);
+		policy.setTotalPremium(12000.00);
+		policy.setType("LIFE");
+		
+		
+		
+		policyModel.setPolicyId("POL_CRP_3");
+		policyModel.setAgeLimit("18-70");
+		policyModel.setPolicyName("LIFE INSURANCE");
+		policyModel.setMaturityPeriod(5);
+		policyModel.setPremium(400.00);
+		policyModel.setSumInsured(500000.00);
+		policyModel.setTotalPremium(12000.00);
+		policyModel.setType("LIFE");
 		
 		userPolicy.setUserPolicyId("USER_PRI_2-POL_CRP_3-1");
 		userPolicy.setPolicyId(policy.getPolicyId());
@@ -68,6 +95,12 @@ public class UserServiceImplTest
 		userPolicy.setStatus("ACTIVE");
 		
 		userPolicyModel.setUserPolicyId("USER_PRI_2-POL_CRP_3-1");
+		userPolicyModel.setPolicyId(policy.getPolicyId());
+		userPolicyModel.setUserId(user.getUserId());
+		userPolicyModel.setDateOfPurchase(LocalDate.of(2019, 8, 1));
+		userPolicyModel.setStatus("ACTIVE");
+		
+		userPolicyModelList.add(userPolicyModel);
 		
 		optionalUser = Optional.of(user);
 	}
@@ -116,4 +149,14 @@ public class UserServiceImplTest
 		assertEquals("USER_PRI_2-POL_CRP_3-1", purchasePolicy.getUserPolicyId());
 	}
 
+	
+	@Test
+	public void testPdfUserPolicyFileReport() throws PolicyException, DocumentException
+	{
+		Mockito.when(policyServiceMock.policyDetails(Mockito.anyString())).thenReturn(policyModel);
+		Mockito.when(userRepositoryMock.findById(Mockito.anyString())).thenReturn(optionalUser);
+		ByteArrayInputStream pdfUserPolicyFileReport = userServiceImpl.pdfUserPolicyFileReport(userPolicyModelList);
+		
+		assertNotNull(pdfUserPolicyFileReport);
+	}
 }
