33a0eaa07a874f7a3e648be1206ea58ce654517c
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-08-01 12:27:52 +0530

    user service

---
+++ src/main/java/com/policy/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.policy.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.policy.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
